7ca40e7e102aea7158e2dbdc7d20bb92a11135b4
ALEN TL <alentl5000@gmail.com>
2021-03-06 15:52:06 +0530

    RashmikaBot

--- RashmikaBot/modules/helper_funcs/chat_status.py
+++ RashmikaBot/modules/helper_funcs/chat_status.py
@@ -409,6 +409,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from SaitamaRobot.modules import connection
+from RashmikaBot.modules import connection
 
 connected = connection.connected
