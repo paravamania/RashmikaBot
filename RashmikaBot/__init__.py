e8be51a394246060b4ef780905f671b667bbcb31
ALEN TL <alentl5000@gmail.com>
2021-03-06 15:33:04 +0530

    RashmikaBot

--- RashmikaBot/__init__.py
+++ RashmikaBot/__init__.py
@@ -200,7 +200,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from SaitamaRobot.modules.helper_funcs.handlers import (CustomCommandHandler,
+from RashmikaBot.modules.helper_funcs.handlers import (CustomCommandHandler,
                                                         CustomMessageHandler,
                                                         CustomRegexHandler)
 
